e3baf0c8e7755a56fc21a37ac1c1e29f764be236
colinj15 <144834836+colinj15@users.noreply.github.com>
2024-11-06 12:54:48 -0600

    Update morrissolver.hs

--- morrissolver.hs
+++ morrissolver.hs
@@ -1,27 +1,27 @@
 data Player = Black | White
 
 Winner = Maybe Player
-//Nothing if there’s a tie
+--Nothing if there’s a tie
 
 Mill = Maybe Player
-//Nothing if no player has a mill
+--Nothing if no player has a mill
 
 Mills = [Mill]
 
 Point = (Maybe Piece, [Point])
-//Piece is nothing if no piece is on that space. The list of points contains adjacent points.
+--Piece is nothing if no piece is on that space. The list of points contains adjacent points.
 
 Game = [Point]
 
 Piece = (Player, Maybe Point)
-//Point is nothing if the piece is removed.
+--Point is nothing if the piece is removed.
 
 Pieces = [Piece]
 
 data Turn = Move ((Point, Point), Bool) | ReMove = (Point, Piece)
-//Move is a tuple of the 2 points that are changed in a move (ex: moving a piece off of one point 
-and onto another). Returns true if that move will give the current player a morris
-//ReMove is for removing a piece if you have a morris
+--Move is a tuple of the 2 points that are changed in a move (ex: moving a piece off of one point 
+--and onto another). Returns true if that move will give the current player a mill
+--ReMove is for removing a piece if you have a mill
 
 legalMoves :: Game -> [Move]
 
