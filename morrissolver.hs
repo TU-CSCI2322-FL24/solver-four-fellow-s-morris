d14064296819d3bd35dc78cfc1707d8be97a6d4e
eleal1 <eleal1@trinity.edu>
2024-11-06 13:10:09 -0600

    modified solver file

--- morrissolver.hs
+++ morrissolver.hs
@@ -1,27 +1,34 @@
-data Player = Black | White
+data Player = Black | White 
 
-Winner = Maybe Player
-//Nothing if there’s a tie
+data Turn = Place | ReMove 
+--Move is a tuple of the 2 points that are changed in a move (ex: moving a piece off of one point 
+--and onto another). Returns true if that move will give the current player a morris
+--ReMove is for removing a piece if you have a morris
 
-Mill = Maybe Player
-//Nothing if no player has a mill
+type Winner = Maybe Player
+--Nothing if there's a tie
 
-Mills = [Mill]
+type Mill = Maybe Player
+--Nothing if no player has a mill
 
-Point = (Maybe Piece, [Point])
-//Piece is nothing if no piece is on that space. The list of points contains adjacent points.
+type Mills = [Mill]
 
-Game = [Point]
+type Point = (Int, Int, Maybe Player)
+--Piece is nothing if no piece is on that space. The list of points contains adjacent points.
 
-Piece = (Player, Maybe Point)
-//Point is nothing if the piece is removed.
+type Game = [Point]
 
-Pieces = [Piece]
+type Piece = (Player, Maybe Point)
+--Point is nothing if the piece is removed.
 
-data Turn = Move ((Point, Point), Bool) | ReMove = (Point, Piece)
-//Move is a tuple of the 2 points that are changed in a move (ex: moving a piece off of one point 
-and onto another). Returns true if that move will give the current player a morris
-//ReMove is for removing a piece if you have a morris
+type Pieces = [Piece]
+
+type Place = (Point, Point, Bool)
+
+type Remove = (Point, Point)
+
+type Move = (Point, Point)
+
 
 legalMoves :: Game -> [Move]
 
