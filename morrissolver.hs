19f601fe3fa1482a1c49586ef8ac2dba504152b5
Jake Borawski <jborawsk@trinity.edu>
2024-11-11 12:50:04 -0600

    Modified types

--- morrissolver.hs
+++ morrissolver.hs
@@ -1,11 +1,9 @@
-data Player = Black | White 
+data Player = B | W | O
 
 data Turn = Place | Remove 
 --Move is a tuple of the 2 points that are changed in a move (ex: moving a piece off of one point 
 --and onto another). Returns true if that move will give the current player a morris
 --ReMove is for removing a piece if you have a morris
-
-data Move = Up | Down | Left | Right
 
 type Winner = Maybe Player
 --Nothing if there's a tie
@@ -13,26 +11,27 @@ type Winner = Maybe Player
 type Point = (Int, Int)
 --Piece is nothing if no piece is on that space. The list of points contains adjacent points.
 
-type Filled = [(Point, Maybe Player)]
+type Board = [Place]
 
-type Board = [(Point, Maybe Piece)]
-
-type Game = [Point]
-
-type Piece = (Maybe Point, Player)
 --Point is nothing if the piece is removed.
 
-type Pieces = [Piece]
+type Place = (Point, Player)
 
-type Place = (Point, Point, Bool)
+type Remove = Point
 
-type Remove = (Point, Point)
+allPoints = [(1,7), (1,4), (1,1), (2,6), (2,4), (2,2), (3,5), 
+            (3,4), (4,7), (4,6), (4,5), (4,3), (4,2), (4,1), (5,5), 
+            (5,4), (5,3), (6,6), (6,4), (6,2), (7,7), (7,4), (7,1)]
 
-legalMoves :: Game -> [Move]
+
+isLegalMove :: Board -> Place -> Bool
+isLegalMove board move = move `elem` board
+
+legalMoves :: Board -> [Place]
 legalMoves = undefined
 
-makeMove :: Game -> Player -> Turn -> Game
+makeMove :: Board -> Player -> Turn -> Board
 makeMove = undefined
 
-state :: Game -> String
+state :: Board -> String
 state = undefined
