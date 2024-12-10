54e8e73f72a3e07a4031a52c0595c35231b1bcde
Marlozam <mzamora@trinity.edu>
2024-12-10 13:06:50 -0600

    I think this is everything, we hope

--- Morrissolver.hs
+++ Morrissolver.hs
@@ -8,6 +8,7 @@ import System.IO
 import System.Directory (renameFile)
 import Data.Maybe
 import Data.List
+import Data.List.Split
 
 data Player = B | W deriving (Eq, Show)
 
@@ -45,20 +46,22 @@ type Rating = Int
 
 -- board -- current player, have all 9 pieces been placed (True  or False)
 -- Phase 1, normal, how many pieces each player has left
--- Phase 1/3, removing, with how may pieces each player has left 
+-- Phase 1, removing, with how may pieces each player has left 
 -- Phase 2/3, normal or removing
 -- Phase 2/3, removing
 --Int tells us how many pieces we have 
 --Bool is if last piece made a mill or not and then we wwant to remove
-type Phase = Int
+
+type Phase = Int -- bool?
+type TurnCounter = Int
 
 type Game = (Board, Player, Phase, Bool, TurnCounter)
 
 
 allPoints :: [Point]
-allPoints = [(1,7), (4,7), (7,7), (2,6), (6,6), (3,5), (4,5), (5,5),
-                 (1,4), (2,4), (3,4), (5,4), (6,4), (7,4), (3,3), (4,3), (5,3),
-                 (2,2), (6,2), (1,1), (4,1), (7,1)]
+allPoints = [(1,7), (1,4), (1,1), (2,6), (2,4), (2,2), (3,5),
+            (3,4), (3,3), (4,7), (4,5), (4,3), (4,1), (5,5),
+            (5,4), (5,3), (6,6), (6,4), (6,2), (7,7), (7,4), (7,1)]
 
 -- there must be an easier way then hardcoding all the 
 -- when have a mill can remove those edges 
@@ -104,15 +107,15 @@ allMills = [ [(1,1), (1,4), (1,7)],
              [(1,7), (4,7), (7,7)] ]
 
 
+newGame :: Player -> Game
+newGame player = (makeBoard allPoints, player, 1, False, 0)
+
 
 -- need to keep track of mills
 -- wanna check after every turn if we have a mill
 -- can do this by checking if there are two edges that are conected 
     -- so if the y in one cordiante is the same as the x in the other and vice versa 
     -- lookUp point (x, y) && lookup 
-
-newGame :: Player -> Game
-newGame player = (makeBoard allPoints, player, 1, False, 0)
 
 isMill :: Point -> Board -> Maybe Player -> Bool
 isMill pieceLoc board pl =
@@ -126,8 +129,8 @@ isMill pieceLoc board pl =
 
 --determine who is gonna win the game 
 --either the oppnoet only has 2 pices left or they have no more legal moves 
-gameWinner :: Game -> Winner
-gameWinner (board, player, _, _)=
+gameWinner :: Game -> Result
+gameWinner (board, player, _, _, _)=
     let playerPieces =  length [ pos| (pos, ply) <- board, ply == Just (opponent player)]
         anyLegalMoves = length (legalPlaces board)
     in if playerPieces <= 2 || anyLegalMoves < 1 then Over (Win player) else Ongoing
@@ -136,6 +139,15 @@ opponent :: Player -> Player
 opponent B = W
 opponent W = B
 
+
+-- want to count how many pices of each payer are on the board 
+--pieces :: PLayer -> Borad -> 
+
+-- have to replace the O with soemthing else
+orderedPoints :: [Point]
+orderedPoints = [(1,7), (4,7), (7,7), (2,6), (6,6), (3,5), (4,5), (5,5),
+                 (1,4), (2,4), (3,4), (5,4), (6,4), (7,4), (3,3), (4,3), (5,3),
+                 (2,2), (6,2), (1,1), (4,1), (7,1)]
 
 makeBoard :: [Point] -> Board
 makeBoard allPoints = [(x, Nothing) | x <- allPoints]
@@ -205,52 +217,46 @@ legalPlaces :: Board -> [Place]
 legalPlaces board = [place | place <- board, isOpen place]
 
 
---Need to add error checking to make sure point is a legal point and on an open space in the board
---Is there a way to set a default value?
---Do we need to change this in order to make it incorporate a game return as opposed to board?
---We need to rewrite this function tbh
+
+
+-- did pattern match instead as well as included the error handling \
+-- add +1 for every turn and once get to 200 the game should output end 
 makeMove :: Game -> Action -> Game
-makeMove game action =
-    let board = getBoard game
-        player = getPlayer game
-        openBoard = getEmptyPlaces game
-    in case action of
-        --instead of using map use concat to remove from list
-        --need to not use O
-        Put point ->
-            let newBoard = map (\(pts, Just ply) -> if pts == point then (pts, Just player) else (pts, Just ply)) openBoard
-            in  (newBoard, opponent player, getPhase game, isMill point newBoard (Just player))
-        Remove point ->
-            let newBoard = map (\(pts, p) -> if pts == point then (pts, Nothing) else (pts,p)) openBoard
-            in  (newBoard, opponent player, getPhase game, getMill game)
-        Move (point1, point2) ->
-            let putGame = makeMove game (Put point2)
-                newGame = makeMove putGame (Remove point1)
-                newBoard = getBoard newGame
-                hasMill = isMill point2 newBoard (Just player)
-            in  (newBoard, opponent player, getPhase game, hasMill)
+makeMove game@(board, player, phase, True, 200) _ = error "Game is over!"
+makeMove game@(board, player, phase, True, turns) (Remove point) =
+    let newBoard = map (\(pts, p) -> if pts == point then (pts, Nothing) else (pts, p)) board
+    in (newBoard, player, phase, False, turns)
+makeMove game@(board, player, phase, True, turns) _ = error "Must be a Remove action"
+makeMove game@(board, player, phase, False, turns) (Remove point) = error "Cannot perform a Remove action in this phase"
+
+makeMove game@(board, player, 1, False, turns) (Put point) =
+    let newBoard = map (\(pts, p) -> if pts == point then (pts, Just player) else (pts, p)) board
+        nextPhase = if turns == 1 then 2 else 1
+    in (newBoard, opponent player, nextPhase, isMill point newBoard (Just player), turns + 1)
+
+makeMove game@(board, player, 2, False, turns) (Move (from, to)) =
+    if not (isLegalMove board (to, Nothing)) then error "Illegal move"
+    else
+        let newBoard = map (\(pts, p) -> if pts == from then (pts, Nothing) else if pts == to then (pts, Just player) else (pts, p)) board
+        in (newBoard, opponent player, 2, isMill to newBoard (Just player), turns)
+
+makeMove _ _ = error "Invalid action"
 
 --Need to adjust types
-allPossibleMoves :: Game -> ([Action], [Action])
-allPossibleMoves game =
-    let player = getPlayer game
-        board = getBoard game
-        moves = legalActions game
-        removes = [Remove (fst pieces) | pieces <- board, snd pieces == Just (opponent player)]
-    in (moves, removes)
+allPossibleMoves :: Game -> [Action]
+allPossibleMoves game@(board, player, phase, False, _) = legalActions game
+allPossibleMoves game@(board, player, phase, True, _) =
+     [Remove (fst pieces) | pieces <- board, snd pieces == Just (opponent player)]
 
 whoWillWin :: Game -> Winner
 whoWillWin game@(board,player,_,_,_)  =
     case gameWinner game of
-        Just winner -> Just winner
-        Nothing ->
-            let board = getBoard game
-                moves = fst $ allPossibleMoves game
+        Over winner -> winner
+        Ongoing ->
+            let moves = allPossibleMoves game
                 newGames = [makeMove game move | move <- moves]
-                millGames = filter getMill newGames
-                removedGames = concat [snd (allPossibleMoves g) | g <- millGames]
-                winners = map (\newGame -> whoWillWin newGame (opponent player)) newGames
-            in if Just player `elem` winners
+                winners = map whoWillWin newGames
+            in bestFor player winners {-if Just player `elem` winners
                 then Just player
                 else if all (== Just (opponent player)) winners
                     then Just (opponent player) 
@@ -292,9 +298,27 @@ countMills game@(board, player, _, _) =
 rateGame :: Game -> Rating
 rateGame game@(board,player,_,_,_) = playerCounter W board - playerCounter B board
 
+boardToString :: String -> Board -> String
+boardToString boardString board =
+    let aux :: String -> [Point] -> String
+        aux [] _ = []
+        aux (x:xs) [] = x : aux xs []
+        aux (x:xs) (p:ps) =
+            if x `elem` "BWO"
+                then case lookupPlayer board p of
+                    Just B  -> 'B' : aux xs ps
+                    Just W  -> 'W' : aux xs ps
+                    Nothing -> 'O' : aux xs ps
+
+                else x : aux xs (p:ps)
+    in aux boardString orderedPoints
+
+getBoardString :: IO String
+getBoardString = readFile "Board.txt"
+
 
 lookupPlayer :: Board -> Point -> Maybe Player
-lookupPlayer [] _ = Nothing  -- Default to Nothing if no player is on a point
+lookupPlayer [] _ = Nothing  -- Default to O if no player is on a point
 lookupPlayer ((pt, player):xs) point
     | pt == point = player
     | otherwise = lookupPlayer xs point
@@ -306,3 +330,55 @@ updateBoardPrint board = do
     let updatedStr = boardToString boardString board
     writeFile "Board.txt" updatedStr
     return updatedStr
+
+
+--Printing
+
+playerString :: Maybe Player -> String
+playerString (Just B) = "B"
+playerString (Just W) = "W"
+playerString Nothing  = "O"
+
+stringPlayer :: String -> Maybe Player
+stringPlayer "B" = Just B
+stringPlayer "W" = Just W
+stringPlayer "O" = Nothing
+stringPlayer x = error (x ++ " is not a valid player (did you make sure that the string is uppercase?)")
+
+playerChar :: Maybe Player -> Char
+playerChar (Just B) = 'B'
+playerChar (Just W) = 'W'
+playerChar Nothing  = 'O'
+
+removeMaybe :: Maybe Player -> Player
+removeMaybe (Just B) = B
+removeMaybe (Just W) = W
+removeMaybe _ = error "Needs to be an actual player"
+
+--String that looks like the game in progress.
+prettyPrint :: Game -> String
+prettyPrint (board, _, _, _, _) = "7 " ++ playerString (lookupPlayer board (1,7)) ++ "        " ++ playerString (lookupPlayer board (4,7)) ++ "        " ++ playerString (lookupPlayer board (7,7)) ++ "\n6    " ++ playerString (lookupPlayer board (2,6)) ++ "     " ++ playerString (lookupPlayer board (4,6)) ++ "     " ++ playerString (lookupPlayer board (6,6)) ++ "\n5       " ++ playerString (lookupPlayer board (3,5)) ++ "  " ++ playerString (lookupPlayer board (4,5)) ++ "  " ++ playerString (lookupPlayer board (5,5)) ++ "\n4 " ++ playerString (lookupPlayer board (1,4)) ++ "  " ++ playerString (lookupPlayer board (2,4)) ++ "  " ++ playerString (lookupPlayer board (3,4)) ++ "     " ++ playerString (lookupPlayer board (5,4)) ++ "  " ++ playerString (lookupPlayer board (6,4)) ++ "  " ++ playerString (lookupPlayer board (7,4)) ++ "\n3       " ++ playerString (lookupPlayer board (3,3)) ++ "  " ++ playerString (lookupPlayer board (4,3)) ++ "  " ++ playerString (lookupPlayer board (5,3)) ++ "\n2    " ++ playerString (lookupPlayer board (2,2)) ++ "     " ++ playerString (lookupPlayer board (4,2)) ++ "     " ++ playerString (lookupPlayer board (6,2)) ++ "\n1 " ++ playerString (lookupPlayer board (1,1)) ++ "        " ++ playerString (lookupPlayer board (4,1)) ++ "        " ++ playerString (lookupPlayer board (7,1)) ++ "\n  1  2  3  4  5  6  7\n"
+
+--For testing IO in GHCi
+testOut :: Game -> IO ()
+testOut g = putStrLn (prettyPrint g)
+
+--String that is put in file
+pickle :: Game -> String
+pickle (board, player, phase, mill, turn) = 
+    [playerChar (snd s) | s <- board] ++ " " 
+    ++ playerString (Just player) ++ " " 
+    ++ show phase ++ " " 
+    ++ show mill ++ " "
+    ++ show turn
+
+--Game that is read out
+unpickle :: String -> Game
+unpickle string = 
+    let [b, pl, ph, m, t] = splitOn " " string
+        board  = zip allPoints [stringPlayer [x] | x <- b]
+        player = stringPlayer pl
+        phase  = read ph :: Int
+        mill   = read m :: Bool
+        turn   = read t :: Int
+    in  (board, removeMaybe player, phase, mill, turn)
